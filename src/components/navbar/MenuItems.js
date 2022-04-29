30b63ae8a47bcf3b79548aa4ebdeff5700993701
ArtJSON <artur.pietrzak1@outlook.com>
2022-04-30 00:03:49 +0200

    Create navbar

---
+++ src/components/navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+  {
+    title: "Home",
+    url: "",
+    className: "nav-links",
+  },
+  {
+    title: "Skills",
+    url: "",
+    className: "nav-links",
+  },
+  {
+    title: "Portfolio",
+    url: "",
+    className: "nav-links",
+  },
+  {
+    title: "About me",
+    url: "",
+    className: "nav-links",
+  },
+  {
+    title: "Contact",
+    url: "",
+    className: "nav-links",
+  },
+];
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,8 +1,44 @@
 import React, { Component } from "react";
+import { MenuItems } from "./MenuItems";
 
 class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      expanded: false,
+    };
+    this.hadnleClick = this.hadnleClick.bind(this);
+  }
+
+  hadnleClick() {
+    this.setState({
+      expanded: !this.state.expanded,
+    });
+  }
+
   render() {
-    return <div></div>;
+    return (
+      <nav className="navbar">
+        <h1 className="navbar-title">Artur Pietrzak</h1>
+        <div>
+          <i
+            onClick={this.hadnleClick}
+            className={this.state.expanded ? "fas fa-bars" : "fas fa-times"}
+          ></i>
+        </div>
+        <ul>
+          {MenuItems.map((l, index) => {
+            return (
+              <li key={index}>
+                <a className={l.className} href={l.url}>
+                  {l.title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    );
   }
 }
 
