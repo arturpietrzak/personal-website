80425ec75387f9fdd6b1ac01b2863b74b4a127c9
ArtJSON <artur.pietrzak1@outlook.com>
2022-05-01 18:53:10 +0200

    Begin styling for desktops

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -24,15 +24,17 @@ class MainPage extends Component {
               <img className="technology-img" src="imgs/projects/weather.png" />
             </div>
             <div className="info-container">
-              <h2 className="project-name">React weather app</h2>
-              <p className="project-description">
-                Web app inspired by iOS weather app, provides information about
-                weather at specified location using Visual Crossing's Weather
-                API.
-              </p>
-              <p className="project-technologies">
-                JavaScript, React, Axios, CSS
-              </p>
+              <div>
+                <h2 className="project-name">React weather app</h2>
+                <p className="project-description">
+                  Web app inspired by iOS weather app, provides information
+                  about weather at specified location using Visual Crossing's
+                  Weather API.
+                </p>
+                <p className="project-technologies">
+                  JavaScript, React, Axios, CSS
+                </p>
+              </div>
               <div className="project-options">
                 <a
                   href="https://react-weather-app-artjson.vercel.app/"
@@ -52,21 +54,20 @@ class MainPage extends Component {
 
           <div className="project">
             <div className="image-container">
-              <img
-                className="technology-img"
-                src="imgs/projects/racetrackranking.png"
-              />
+              <img src="imgs/projects/racetrackranking.png" />
             </div>
             <div className="info-container">
-              <h2 className="project-name">Race track ranking</h2>
-              <p className="project-description">
-                Webpage project created to provide information about race
-                tracks. Users can create own race tracks, or review existing
-                ones.
-              </p>
-              <p className="project-technologies">
-                JavaScript, Node, Express, MongoDB, EJS, Sass
-              </p>
+              <div>
+                <h2 className="project-name">Race track ranking</h2>
+                <p className="project-description">
+                  Webpage project created to provide information about race
+                  tracks. Users can create own race tracks, or review existing
+                  ones.
+                </p>
+                <p className="project-technologies">
+                  JavaScript, Node, Express, MongoDB, EJS, Sass
+                </p>
+              </div>
               <div className="project-options">
                 <a
                   href="https://race-track-ranking.vercel.app/"
@@ -89,15 +90,17 @@ class MainPage extends Component {
               <img className="technology-img" src="imgs/projects/sudoku.png" />
             </div>
             <div className="info-container">
-              <h2 className="project-name">React Sudoku</h2>
-              <p className="project-description">
-                Game of Sudoku, with option to generate new boards with
-                different level of difficulty. If the game is too hard, there is
-                built in solver to provide correct numbers.
-              </p>
-              <p className="project-technologies">
-                JavaScript, Node, React, CSS
-              </p>
+              <div>
+                <h2 className="project-name">React Sudoku</h2>
+                <p className="project-description">
+                  Game of Sudoku, with option to generate new boards with
+                  different level of difficulty. If the game is too hard, there
+                  is built in solver to provide correct numbers.
+                </p>
+                <p className="project-technologies">
+                  JavaScript, Node, React, CSS
+                </p>
+              </div>
               <div className="project-options">
                 <a
                   href="https://react-sudoku-dun.vercel.app/"
@@ -123,12 +126,14 @@ class MainPage extends Component {
               />
             </div>
             <div className="info-container">
-              <h2 className="project-name">Commutely</h2>
-              <p className="project-description">
-                Team project created with mission to educate people on true
-                costs of car transport, both economic and enviromental.
-              </p>
-              <p className="project-technologies">JS, Bootstrap</p>
+              <div>
+                <h2 className="project-name">Commutely</h2>
+                <p className="project-description">
+                  Team project created with mission to educate people on true
+                  costs of car transport, both economic and enviromental.
+                </p>
+                <p className="project-technologies">JS, Bootstrap</p>
+              </div>
               <div className="project-options">
                 <a
                   href="https://commutely-webpage.vercel.app/"
