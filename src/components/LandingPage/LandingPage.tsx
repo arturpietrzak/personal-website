fc08c596615d0946380f9209a7c57da937fe2781
ArtJSON <artur.pietrzak1@outlook.com>
2022-12-28 21:57:58 +0100

    Add FadeIn component to the LandingPage

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -4,22 +4,30 @@ import { ReactComponent as LinkedinLogo } from "../../assets/icons/linkedin.svg"
 import { ReactComponent as GithubLogo } from "../../assets/icons/github.svg";
 import { ReactComponent as EmailLogo } from "../../assets/icons/email.svg";
 import "./LandingPage.scss";
+import { useRef } from "react";
+import FadeIn from "../FadeIn/FadeIn";
 
 export default function LandingPage() {
+  useRef();
+
   return (
     <div className="landing-page">
       <header className="header">
         <div className="header__background" />
-        <div className="header__content mw">
-          <div className="header__content__text">
-            <h1>Artur Pietrzak</h1>
-            <p>Frontend developer</p>
+        <FadeIn>
+          <div className="header__content mw">
+            <div className="header__content__text">
+              <h1>Artur Pietrzak</h1>
+              <p>Change maker</p>
+              <p>Future thinker</p>
+              <p>Frontend developer</p>
+            </div>
+            <div className="header__content__preview-container">
+              <div className="header__content__preview-container__desktop-preview" />
+              <div className="header__content__preview-container__mobile-preview" />
+            </div>
           </div>
-          <div className="header__content__preview-container">
-            <div className="header__content__preview-container__desktop-preview" />
-            <div className="header__content__preview-container__mobile-preview" />
-          </div>
-        </div>
+        </FadeIn>
       </header>
       <section className="about-me mw">
         <img className="about-me__image" src="/profilepic.jpg" />
@@ -34,37 +42,43 @@ export default function LandingPage() {
         </div>
       </section>
       <section className="porftolio mw">
-        <ProjectTile
-          description="Having struggled with understanding how the Spotify OAuth flow works, I made the course I wish I could have had.
+        <FadeIn>
+          <ProjectTile
+            description="Having struggled with understanding how the Spotify OAuth flow works, I made the course I wish I could have had.
             Unlike tutorials that only cover a few concepts and leave you with half-baked GitHub repositories, this course covers everything from explaining the principles of REST APIs to implementing Spotify's OAuth flow and fetching API data in a React app. By the end of the course, you’ll have an app deployed to the internet you can add to your portfolio.
             "
-          githubUrl="dasdsa"
-          imgUrl="/weather.png"
-          name="Weather app"
-          deploymentUrl=""
-          technologies={["dsadsa", "dsadsa", "dsadsa"]}
-        />
-        <ProjectTile
-          description="Having struggled with understanding how the Spotify OAuth flow works, I made the course I wish I could have had.
+            githubUrl="dasdsa"
+            imgUrl="/weather.png"
+            name="Weather app"
+            deploymentUrl=""
+            technologies={["dsadsa", "dsadsa", "dsadsa"]}
+          />
+        </FadeIn>
+        <FadeIn>
+          <ProjectTile
+            description="Having struggled with understanding how the Spotify OAuth flow works, I made the course I wish I could have had.
             Unlike tutorials that only cover a few concepts and leave you with half-baked GitHub repositories, this course covers everything from explaining the principles of REST APIs to implementing Spotify's OAuth flow and fetching API data in a React app. By the end of the course, you’ll have an app deployed to the internet you can add to your portfolio.
             "
-          githubUrl="dasdsa"
-          imgUrl="/weather.png"
-          name="Weather app"
-          deploymentUrl=""
-          technologies={["dsadsa", "dsadsa", "dsadsa"]}
-          inverted
-        />
-        <ProjectTile
-          description="Having struggled with understanding how the Spotify OAuth flow works, I made the course I wish I could have had.
+            githubUrl="dasdsa"
+            imgUrl="/weather.png"
+            name="Weather app"
+            deploymentUrl=""
+            technologies={["dsadsa", "dsadsa", "dsadsa"]}
+            inverted
+          />
+        </FadeIn>
+        <FadeIn>
+          <ProjectTile
+            description="Having struggled with understanding how the Spotify OAuth flow works, I made the course I wish I could have had.
             Unlike tutorials that only cover a few concepts and leave you with half-baked GitHub repositories, this course covers everything from explaining the principles of REST APIs to implementing Spotify's OAuth flow and fetching API data in a React app. By the end of the course, you’ll have an app deployed to the internet you can add to your portfolio.
             "
-          githubUrl="dasdsa"
-          imgUrl="/weather.png"
-          name="Weather app"
-          deploymentUrl=""
-          technologies={["dsadsa", "dsadsa", "dsadsa"]}
-        />
+            githubUrl="dasdsa"
+            imgUrl="/weather.png"
+            name="Weather app"
+            deploymentUrl=""
+            technologies={["dsadsa", "dsadsa", "dsadsa"]}
+          />
+        </FadeIn>
       </section>
       <section className="contact">
         <p className="contact__prompt">Let's work together!</p>
@@ -74,8 +88,11 @@ export default function LandingPage() {
         </button>
       </section>
       <section className="social-info mw">
-        <SocialButton icon={<LinkedinLogo />} url="dsadas" />
-        <SocialButton icon={<GithubLogo />} url="dsadas" />
+        <p className="social-info__signature">Artur Pietrzak</p>
+        <div className="social-info__buttons">
+          <SocialButton icon={<LinkedinLogo />} url="dsadas" />
+          <SocialButton icon={<GithubLogo />} url="dsadas" />
+        </div>
       </section>
     </div>
   );
