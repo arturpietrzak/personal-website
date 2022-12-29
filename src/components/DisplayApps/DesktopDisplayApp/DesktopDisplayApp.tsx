7684b572950bea39d52f27d0a485aeec3d2d04af
ArtJSON <artur.pietrzak1@outlook.com>
2022-12-29 02:48:33 +0100

    Create previews for desktop and mobile apps

---
+++ src/components/DisplayApps/DesktopDisplayApp/DesktopDisplayApp.tsx
@@ -0,0 +1,3 @@
+export default function DesktopDisplayApp() {
+  return <div></div>;
+}
---
+++ src/components/DisplayApps/MobileDisplayApp/MobileDisplayApp.tsx
@@ -0,0 +1,15 @@
+import "./MobileDisplayApp.scss";
+
+export default function MobileDisplayApp() {
+  return (
+    <div className="mobile-display-app">
+      <div className="mobile-display-app__navbar">
+        <div className="mobile-display-app__navbar__options" />
+        <div className="mobile-display-app__navbar__profile" />
+      </div>
+      <div className="mobile-display-app__options-container">
+        <div className="mobile-display-app__options-container" />
+      </div>
+    </div>
+  );
+}
