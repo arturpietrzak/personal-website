37cc97897bb861acb72d3ab6bfab973324b525da
arturpietrzak <artur.pietrzak1@outlook.com>
2026-06-21 01:15:46 +0200

    Update

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -53,7 +53,7 @@ export default async function RootLayout({
           sizes="any"
         />
         <meta name="theme-color" content="#090a0f" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
       </head>
       <body className={inter.className}>
         <NextIntlClientProvider locale={locale} messages={messages}>
